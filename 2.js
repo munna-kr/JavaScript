e8b906b0a0cf0b5d8dfeeef534a09b2837695d2f
Mk <mk142532@gmail.com>
2025-11-04 04:11:18 +0530

    updates

--- 2.js
+++ 2.js
@@ -1,17 +1,1 @@
-'use strict';
-
-const input = require('prompt-sync')();
-
-console.log('-Calculator for addition-');
-console.log('Enter Numbers...(0 to quit)');
-const numbers = []; 
-
-let add = 0;
-let i = 0;
-while (numbers[i] !== '0') {
-  i++;
-  numbers[i] = input();
-  console.log('+');
-  add = add + Number(numbers[i]);
-}
-console.log('Sum = ', add);
+console.log('This is file 2.js');
