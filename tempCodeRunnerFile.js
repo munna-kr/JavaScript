80b0dc109b946b7972e188aa85045ec0058a2888
Mk <mk142532@gmail.com>
2025-11-04 04:45:37 +0530

    stop tracking

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const objHuman = {
-  sciName: 'HomoSapiens',
-  category: 'Living-Things',
-  origin: 'Earth',
-};
-objHuman.sciName = '1';
-objHuman.newproperty = 'walking';
-delete objHuman.sciName;
-console.log(objHuman);
