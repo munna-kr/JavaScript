79e63a90d64cda51825f90cc9306dacf408bfe6b
Mk <mk142532@gmail.com>
2025-11-04 04:47:12 +0530

    update

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+    std::cout<<"No Place is like 127.0.0.1"<<std::endl;
+
+    int arrN[] = {12, 13, 10, 11};
+    std::cout<<arrN[3]<<std::endl;
+    std::cout<<sizeof(arrN)/sizeof(arrN[0])<<std::endl;
+    int age = 10;
+    std::cout<<sizeof(age)<<std::endl;
+    return 0;
+}
