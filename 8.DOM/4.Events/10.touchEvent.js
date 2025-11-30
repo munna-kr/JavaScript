f2555ce19ec1efef6aa2b59ea57ac20e32ac8c46
Mk <mk142532@gmail.com>
2025-12-01 05:22:55 +0530

    1dec2025

--- 8.DOM/4.Events/10.touchEvent.js
+++ 8.DOM/4.Events/10.touchEvent.js
@@ -1,2 +1,6 @@
 const btn = document.getElementById('btn');
 btn.addEventListener('touchstart', () => console.log('touch started..'));
+
+btn.addEventListener('touchmove', () => console.log('touch moved...'));
+
+btn.addEventListener('touchend', () => console.log('finger lifted'));
---
+++ 8.DOM/4.Events/11.eventHandle.js
@@ -0,0 +1,3 @@
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => console.log('Hello world1'));
+btn.addEventListener('click', () => console.log('Hello world2'));
