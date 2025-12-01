6b2e4f7e2531a542b06802d455ea495ca832942a
mk <mk142532@gmail.com>
2025-12-01 16:21:58 +0530

    00

--- user_input.js
+++ user_input.js
@@ -1,17 +1,18 @@
-'use strict';
+"use strict";
 
-const input = require('prompt-sync')();
+const input = require("prompt-sync")();
 
-console.log('-Calculator for addition-');
-console.log('Enter Numbers...(0 to quit)');
-const numbers = []; 
+console.log("-Calculator for addition-");
+console.log("Enter Numbers...(0 to quit)");
+const numbers = [];
 
 let add = 0;
 let i = 0;
-while (numbers[i] !== '0') {
+while (numbers[i] !== "0") {
   i++;
   numbers[i] = input();
-  console.log('+');
+  console.log("+");
   add = add + Number(numbers[i]);
 }
-console.log('Sum = ', add);
+console.log("Sum = ", add);
+console.log("end");
